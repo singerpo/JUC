4bf324235d5f55184b32ca0d8eda3ad55f8b0f2a
singerpo <85261303@qq.com>
2022-05-16 03:19:14 +0800

    JVM

---
+++ src/main/java/com/sing/jvm/basic/Basic.java
@@ -0,0 +1,14 @@
+package com.sing.jvm.basic;
+
+/**
+ * 常见的JVM实现
+ * 1.Hotspot:oracle官方，我们做实验用的JVM
+ * 2.azul zing:最新垃圾回收的业界标杆（www.azul.com)
+ * 3.TaobaoVM:hotspot深度定制版
+ * 4.LiquidVM：直接针对硬件
+ * 5.J9:IBM
+ * 6.Microsoft VM
+ * 7.jrockit:BEA,曾经号称世界上最快的JVM,被Oracle收购合并于hotspot
+ */
+public class Basic {
+}
