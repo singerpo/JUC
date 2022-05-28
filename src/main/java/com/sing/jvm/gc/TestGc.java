7426e522c1d506a1e8f5830684607541391fdb31
singerpo <852361303@qq.com>
2022-05-28 18:41:06 +0800

    add runtime data area

---
+++ src/main/java/com/sing/jvm/gc/TestGc.java
@@ -0,0 +1,51 @@
+package com.sing.jvm.gc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**110:1.30
+ *
+ * 熟悉GC常用算法，熟悉常见垃圾收集器，具有实际JVM调优实战经验
+ * -.找出垃圾的方法
+ * ①引用计数法（无法解决循环引用问题）
+ * ②Root Searching(根可达算法或根搜索算法）
+ * --GC roots(根）包含：JVM stack,native method stack,runtime constant pool,
+ *   static references in method area,Clazz(也就是线程栈变量、静态变量、常量池、JNI指针)
+ *
+ * 二.垃圾回收算法
+ * ①Mark-Sweep(标记清除）
+ * ②Copying（拷贝）
+ * ③Mark-Compact(标记压缩）
+ *
+ * 三.JVM内存分代模型（用于分代垃圾回收算法）
+ * 1.除ZGC Epsilon Shenandoah之外的GC都是使用分代模型
+ *  G1是逻辑分代，物理不分代
+ *  除此之外的分代模型，不仅逻辑分代而且物理也分代
+ * 2.虚拟机对象分配流程：尝试栈上分配→尝试线程本地分配TLAB→是否满足进入老年代→eden分配
+ * 3，对象何时进入老年代？
+ * --超过XX:MaxTenuringThreshold指定次数（CMS默认6其他都是默认15）
+ * --大对象直接进入(-XX:PretenureSizeThreshold 默认值为0）
+ * --垃圾回收时Edan+S1存活对象超过S2的内存空间50%(TargetSurviveRatio)时 超过的所有年龄段进入
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class TestGc {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+            if(i == 0){
+                list.remove(0);
+            }
+        }
+
+    }
+}
--- src/main/java/com/sing/jvm/runtime/TestIPlusPlus.java
+++ src/main/java/com/sing/jvm/runtime/TestIPlusPlus.java
@@ -1,15 +1,39 @@
 package com.sing.jvm.runtime;
 
 /**
- * 2.01
  * 1.Java运行时数据区
  * 包含堆、栈、方法区、本地方法栈、程序计数器、直接内存
  * 2.栈包含多个栈帧，一个栈帧包含局部变量表、操作数栈、帧数据区（包含常量池指针、异常处理表、返回值）
- * 3.创建对象指令
- *  --new #2  //<com.xx.xx> 申请内存、成员变量赋默认值，压栈
- *  --dup
- *  --invokespecial #3  //<com.xx.xx.<init>> 成员表里赋初始值、执行构造方法
- *  --astore_1 赋值
+ * 3.方法区
+ * (1) Perm Space(<1.8)
+ * 字符串常量位于PermSpace
+ * FGC不会清理
+ * 大小启动的时候指定，不能变
+ * （2）Meta Space(>=1.8)
+ * 字符串常量位于堆
+ * 会触发FGC清理
+ * 不设定的话，最大就是物理内存
+ * <p>
+ * 4.创建对象指令
+ * --new #2  //<com.xx.xx> 申请内存、成员变量赋默认值，压栈
+ * --dup
+ * --invokespecial #3  //<com.xx.xx.<init>> 成员表里赋初始值、执行构造方法
+ * --astore_1 赋值
+ * <p>
+ * 5.常用指令
+ * store 赋值
+ * load  压栈
+ * invoke
+ * --invokestatic
+ * 执行静态方法
+ * --invokevirtual
+ * 执行方法
+ * --invokeinterface
+ * 执行接口方法
+ * --invokespecial
+ * 可以直接定位，不需要多态的方法（private方法，构造方法）
+ * --invokedynamic
+ * lambda表达式、反射、其他动态语言scala kotlin、CGLIB ASM,动态产生的class,会用到该指令
  */
 public class TestIPlusPlus {
 
