a5c447ed7aa5f8e4c1881f2af5e219d3b426e9f8
singerpo <852361303@qq.com>
2022-06-01 20:16:47 +0800

    update jvm tuning

--- src/main/java/com/sing/jvm/gc/TestTuning.java
+++ src/main/java/com/sing/jvm/gc/TestTuning.java
@@ -3,13 +3,14 @@ package com.sing.jvm.gc;
 import java.util.LinkedList;
 import java.util.List;
 
-/**114:1.18
+/**114:1.27
  * @author songbo
  * @since 2022-05-31
  */
 public class TestTuning {
 
     public static void main(String[] args) {
+
         List list = new LinkedList();
         for (/*int i = 1*/; ; /*i++*/) {
             byte[] b = new byte[1024 * 1024];
