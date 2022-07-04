714ac2426399b834170bd464ec7b56cb276af53f
singerpo <852361303@qq.com>
2022-07-04 18:31:06 +0800

    add Observer

---
+++ src/main/java/com/sing/design/b03observer/ObserverMain.java
@@ -0,0 +1,14 @@
+package com.sing.design.b03observer;
+
+/**
+ * https://blog.csdn.net/a745233700/article/details/83662714
+ * 观察者模式
+ * 又称为 发布-订阅模式，定义了对象之间一对多依赖关系，当目标对象(被观察者)的状态发生改变时，它的所有依赖者(观察者)都会收到通知。
+ * @author songbo
+ * @since 2022-07-04
+ */
+public class ObserverMain {
+    public static void main(String[] args) {
+
+    }
+}
