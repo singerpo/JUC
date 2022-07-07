45207ec02c08186d673ea580811c0d10a3d4e334
singerpo <852361303@qq.com>
2022-07-07 11:22:44 +0800

    update ChainOfResponsibility

--- src/main/java/com/sing/design/b04chainofresponsibility/FilterMain.java
+++ src/main/java/com/sing/design/b04chainofresponsibility/FilterMain.java
@@ -9,54 +9,90 @@ import java.util.List;
  */
 public class FilterMain {
     public static void main(String[] args) {
+        Request request = new Request("request");
+        Response response = new Response("response");
+        FilterChain filterChain = new FilterChain();
+        filterChain.addFilter(new HtmlFilter()).addFilter(new WordFilter())
+                .doFilter(request,response);
+
 
     }
 }
 
 interface Filter {
-    boolean doFilter(Request request, Response response,FilterChain filterChain);
+    boolean doFilter(Request request, Response response, FilterChain filterChain);
 }
 
 class Request {
     String str;
+
+    public Request(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "str='" + str + '\'' +
+                '}';
+    }
 }
 
 class Response {
     String str;
+
+    public Response(String str) {
+        this.str = str;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "str='" + str + '\'' +
+                '}';
+    }
 }
 
-class FilterChain implements Filter{
+class FilterChain  {
     List<Filter> filters = new ArrayList<>();
-    int index = 0;
+    int index = -1;
 
-    public Filter addFilter(Filter filter){
+    public FilterChain addFilter(Filter filter) {
         filters.add(filter);
         return this;
     }
 
-    @Override
-    public boolean doFilter(Request request, Response response,FilterChain filterChain) {
-        boolean flag = filters.get(index).doFilter(request,response,filterChain);
+
+    public boolean doFilter(Request request, Response response) {
         index++;
-        return flag;
+        if (index >= filters.size()) {
+            return true;
+        }
+        boolean result = filters.get(index).doFilter(request, response, this);
+
+        return result;
     }
 }
 
-class HtmlFilter implements Filter{
+class HtmlFilter implements Filter {
 
     @Override
-    public boolean doFilter(Request request, Response response,FilterChain filterChain) {
-        System.out.println(request);
-        filterChain.doFilter(request,response,filterChain);
-        System.out.println(response);
-        return false;
+    public boolean doFilter(Request request, Response response, FilterChain filterChain) {
+        System.out.println("HtmlFilter" + request);
+        filterChain.doFilter(request, response);
+        System.out.println("HtmlFilter" + response);
+        return true;
     }
 }
 
-class WordFilter implements Filter{
+class WordFilter implements Filter {
 
     @Override
-    public boolean doFilter(Request request, Response response,FilterChain filterChain) {
-        return false;
+    public boolean doFilter(Request request, Response response, FilterChain filterChain) {
+        System.out.println("WordFilter" + request);
+        filterChain.doFilter(request, response);
+        System.out.println("WordFilter" + response);
+        return true;
     }
 }
