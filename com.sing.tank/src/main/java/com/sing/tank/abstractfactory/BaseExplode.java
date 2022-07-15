989b1ec27e3d7ac99955233bd5e708a67c15df0f
singerpo <852361303@qq.com>
2022-07-15 16:43:39 +0800

    update com.sing.tank

--- com.sing.tank/src/main/java/com/sing/tank/abstractfactory/BaseExplode.java
+++ com.sing.tank/src/main/java/com/sing/tank/abstractfactory/BaseExplode.java
@@ -1,8 +1,12 @@
 package com.sing.tank.abstractfactory;
 
+import java.awt.*;
+
 /**
  * @author songbo
  * @since 2022-07-15
  */
 public abstract class BaseExplode {
+
+    public abstract void paint(Graphics graphics);
 }
