f8219153d3ef1c4ce6d937047745f137f7bce87a
test <test@qq.com>
2022-07-26 21:36:13 +0800

    update tank

--- com.sing.tank/src/main/java/com/sing/tank/strategy/FourDirectionFireStrategy.java
+++ com.sing.tank/src/main/java/com/sing/tank/strategy/FourDirectionFireStrategy.java
@@ -21,16 +21,17 @@ public class FourDirectionFireStrategy implements FireStrategy {
         int diff = 500;
         if (tank.getPaintCount() > diff / TankFrame.PAINT_DIFF) {
             tank.setPaintCount(0);
-            // if (tank.getGroupEnum() == GroupEnum.GOOD) {
-            //     for (DirectionEnum directionEnum : DirectionEnum.values()) {
-            //         tank.getGameModel().getGameFactory().createBullet(directionEnum, tank);
-            //     }
-            //     new Thread(() -> new Audio("audio/tank_fire.wav").play()).start();
-            // } else {
-            //     tank.getGameModel().getGameFactory().createBullet(tank.getDirectionEnum(), tank);
-            // }
-            BaseBullet baseBullet = GameModel.getInstance().getGameFactory().createBullet(tank.getDirectionEnum(), tank);
-            GameModel.getInstance().add(baseBullet);
+             if (tank.getGroupEnum() == GroupEnum.GOOD) {
+                 for (DirectionEnum directionEnum : DirectionEnum.values()) {
+                     BaseBullet baseBullet = GameModel.getInstance().getGameFactory().createBullet(directionEnum, tank);
+                     GameModel.getInstance().add(baseBullet);
+                 }
+                 new Thread(() -> new Audio("audio/tank_fire.wav").play()).start();
+             } else {
+                 BaseBullet baseBullet = GameModel.getInstance().getGameFactory().createBullet(tank.getDirectionEnum(), tank);
+                 GameModel.getInstance().add(baseBullet);
+             }
+
         }
     }
 }
