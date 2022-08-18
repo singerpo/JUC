299604df1cb01e02c4f940cccc91fad257aea2e0
singerpo <852361303@qq.com>
2022-08-18 16:00:31 +0800

    add tank net

---
+++ com.sing.tank/src/main/java/com/sing/tank/net/Client.java
@@ -0,0 +1,117 @@
+package com.sing.tank.net;
+
+import com.sing.netty.chat.ClientFrame;
+import com.sing.tank.abstractfactory.BaseTank;
+import com.sing.tank.facade.GameModel;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author songbo
+ * @since 2022-08-15
+ */
+public class Client {
+
+    private ChannelFuture channelFuture;
+    private ClientFrame clientFrame;
+
+    public void connect(ClientFrame clientFrame) {
+        this.clientFrame = clientFrame;
+        // 事件处理的线程池
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup(1);
+
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            channelFuture = bootstrap.group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ClientChannelInitializer())
+                    .connect("127.0.0.1", 8686)
+                    .sync();
+
+            // // 如果没有.sync，则添加listener监听是否连接成功
+            // channelFuture.addListener(new ChannelFutureListener() {
+            //     @Override
+            //     public void operationComplete(ChannelFuture channelFuture) throws Exception {
+            //         System.out.println(channelFuture.isSuccess());
+            //
+            //     }
+            // });
+            // channelFuture.sync();
+            // 阻塞等待closeFuture
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+
+    }
+
+    /**
+     * 向服务端发送消息
+     *
+     * @param msg 消息
+     */
+    public void sendMsg(String msg) {
+        ByteBuf byteBuf = null;
+        try {
+            byteBuf = Unpooled.copiedBuffer(msg.getBytes("UTF-8"));
+            this.getChannelFuture().channel().writeAndFlush(byteBuf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 关闭客户端
+     */
+    public void closeClient() {
+        this.sendMsg("b_bye_b");
+    }
+
+    class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            ChannelPipeline channelPipeline = socketChannel.pipeline();
+            channelPipeline.addLast(new TankJoinMsgEncoder())
+                    .addLast(new TankJoinMsgDecoder())
+                    .addLast(new ClientChildHandler());
+        }
+    }
+
+    class ClientChildHandler extends SimpleChannelInboundHandler<TankJoinMsg> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, TankJoinMsg tankJoinMsg) throws Exception {
+            if(tankJoinMsg.id.equals(GameModel.getInstance().getMainTank().getId())){
+                return;
+            }
+            System.out.println(tankJoinMsg);
+            BaseTank baseTank = GameModel.getInstance().getGameFactory().createTank(tankJoinMsg.x,tankJoinMsg.y,tankJoinMsg.directionEnum,tankJoinMsg.groupEnum,tankJoinMsg.repeat);
+            GameModel.getInstance().add(baseTank);
+        }
+    }
+
+    public ChannelFuture getChannelFuture() {
+        return channelFuture;
+    }
+
+    public void setChannelFuture(ChannelFuture channelFuture) {
+        this.channelFuture = channelFuture;
+    }
+
+    public ClientFrame getClientFrame() {
+        return clientFrame;
+    }
+
+    public void setClientFrame(ClientFrame clientFrame) {
+        this.clientFrame = clientFrame;
+    }
+}
+
---
+++ com.sing.tank/src/main/java/com/sing/tank/net/Server.java
@@ -0,0 +1,105 @@
+package com.sing.tank.net;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * @author songbo
+ * @since 2022-08-16
+ */
+public class Server {
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private ServerFrame serverFrame;
+
+    public void startServer(ServerFrame serverFrame) {
+        this.serverFrame = serverFrame;
+        // 只负责连接
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            ChannelFuture channelFuture = serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline channelPipeline = socketChannel.pipeline();
+                            channelPipeline.addLast(new ServerChildHandler());
+
+                        }
+                    })
+                    .bind("127.0.0.1", 8686)
+                    .sync();
+            this.serverFrame.updateServerMsg("server started!");
+            // 阻塞等待closeFuture
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    class ServerChildHandler extends ChannelInboundHandlerAdapter {//SimpleChannelInboundHandler
+
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            Server.clients.add(ctx.channel());
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            try {
+                byte[] bytes = new byte[byteBuf.readableBytes()];
+                // byteBuf.readBytes(bytes);
+                byteBuf.getBytes(byteBuf.readerIndex(), bytes);
+                String clientMsg = new String(bytes, "UTF-8");
+                getServerFrame().updateClientMsg(clientMsg);
+                if ("b_bye_b".equals(clientMsg)) {
+                    System.out.println("客户端要求退出");
+                    // 删除出异常的客户端channel并关闭
+                    Server.clients.remove(ctx.channel());
+                    ctx.close();
+                    return;
+                }
+                // 回复信息给客户端
+                Server.clients.writeAndFlush(msg);
+            } finally {
+                // if (byteBuf != null) {
+                //     // 释放内存
+                //     ReferenceCountUtil.release(byteBuf);
+                // }
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            // 删除出异常的客户端channel并关闭
+            Server.clients.remove(ctx.channel());
+            ctx.close();
+
+        }
+    }
+
+    public ServerFrame getServerFrame() {
+        return serverFrame;
+    }
+
+    public void setServerFrame(ServerFrame serverFrame) {
+        this.serverFrame = serverFrame;
+    }
+}
+
+
+
